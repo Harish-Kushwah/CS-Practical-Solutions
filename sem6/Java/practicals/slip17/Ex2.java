34f25c370396d7d7147817f758719883ec8de6d4
Harish-Kushwah <harishkushwah54321@gmail.com>
2024-04-16 12:15:03 +0530

    java slip18 added

--- sem6/Java/practicals/slip17/Ex2.java
+++ sem6/Java/practicals/slip17/Ex2.java
@@ -1,7 +1,6 @@
 package sem6.Java.practicals.slip17;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
