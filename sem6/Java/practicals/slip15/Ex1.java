039bf46fafe03e2a2c2b68ac420c6c3d3d0e0a3b
Harish-Kushwah <harishkushwah54321@gmail.com>
2024-04-17 17:19:20 +0530

    Java slip 15 ,18 ex1 updated

--- sem6/Java/practicals/slip15/Ex1.java
+++ sem6/Java/practicals/slip15/Ex1.java
@@ -1,11 +1,28 @@
 package sem6.Java.practicals.slip15;
-
 /*1. Write a java program to display name and priority of a Thread. */
+
+class MyThread extends Thread
+{
+    MyThread(String name ,int priority)
+    {
+        this.setName(name);
+        this.setPriority(priority);
+    }
+
+    public void run()
+    {
+        System.out.println("Thread name :"+this.getName());
+        System.out.println("Priority :"+this.getPriority());
+
+    }
+}
 public class Ex1 {
     public static void main(String[] args) {
-        Thread th = new Thread();
-        th.start();
-        System.out.println("Name :"  + th.getName());
-        System.out.println("Priority :"+th.getPriority());
+        MyThread th1  = new MyThread("MyThread1" , 3);
+        MyThread th2  = new MyThread("MyThread2" , 2);
+        MyThread th3  = new MyThread("MyThread3" , 5);
+        th1.start();
+        th2.start();
+        th3.start();
     }
 }
