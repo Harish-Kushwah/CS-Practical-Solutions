a64a10470128b467e154bb919cdca98a16487929
Harish-Kushwah <harishkushwah54321@gmail.com>
2024-04-17 17:56:10 +0530

    Java slip21 ex2 added

---
+++ sem6/Java/practicals/slip21/Ex2.java
@@ -0,0 +1,73 @@
+package sem6.Java.practicals.slip21;
+
+class SharedResources {
+    int count = 0;
+
+    public synchronized void produce() {
+        try {
+            while (count == 5) {
+                wait();
+            }
+            System.out.println("Producer produce the value : " + ++count);
+            notify();
+            Thread.sleep(1000);
+        } catch (Exception exp) {
+
+        }
+    }
+
+    public synchronized void consume() {
+        try {
+            while (count == 0) {
+                wait();
+            }
+            System.out.println("Consumer consumed the value : " + --count);
+
+            notify();
+            Thread.sleep(1000);
+
+        } catch (Exception exp) {
+
+        }
+    }
+}
+
+class Producer extends Thread {
+    SharedResources resources;
+    Producer(SharedResources resources) {
+        this.resources = resources;
+    }
+
+    public void run() {
+        while (true) {
+            resources.produce();
+        }
+    }
+}
+
+class Consumer extends Thread {
+    SharedResources resources;
+    Consumer(SharedResources resources) {
+        this.resources = resources;
+    }
+    public void run() {
+        while (true) {
+            resources.consume();
+        }
+    }
+}
+
+public class Ex2 {
+    public static void main(String[] args) throws InterruptedException {
+        SharedResources resources = new SharedResources();
+
+        Producer producer = new Producer(resources);
+        Consumer consumer = new Consumer(resources);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        consumer.join();
+    }
+}
